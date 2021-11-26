d4a9ba12e323fa54d7ae47ab8279e34b3893adb3
Zolfa Shefreie <z.shefreie@gmail.com>
2021-11-26 10:02:43 +0000

    text cleaner start

--- mapper.py
+++ mapper.py
@@ -1,7 +1,52 @@
 import sys
+import re
 
-for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-    for word in words:
-        print('%s\t%s' % (word, 1))
+
+class TextCleaner:
+    punc = '''!()-[]{.};:'"\,<>/?@#$%^&*_~'''
+    
+    def run(text: str) -> list:
+        """
+            run textCleaner for input string
+        Args:
+            text (str): input string
+
+        Returns:
+            list: token list
+        """
+        pass
+    
+    def tokenize(text: str) -> list:
+        pass
+    
+    @classmethod
+    def normlize(cls, text: str) -> str:
+        """
+
+        Args:
+            text (str): input string
+
+        Returns:
+            str: normlized string
+        """
+        text = text.lower()
+        text = re.sub(r"\d+", '', text)
+        
+        for each in cls.punc:
+            text = text.replace(each, "")
+        
+        text.strip()
+        
+        return text
+        
+    
+    def remove_stopwords(tokens: list) -> list:
+        pass
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line = line.strip()
+        words = line.split()
+        for word in words:
+            print('%s\t%s' % (word, 1))
